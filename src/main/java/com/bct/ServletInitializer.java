d882416c01ba3427bf5befc8fc6c90cbef38adc3
Sean Thatcher <papertray3@gmail.com>
2015-07-16 14:12:11 -0400

    Project Creation:
    Project created via Spring Initializr (http://start.spring.io)
    run with mvn spring-boot:run

---
+++ src/main/java/com/bct/ServletInitializer.java
@@ -0,0 +1,13 @@
+package com.bct;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.context.web.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(ContactsApplication.class);
+	}
+
+}
